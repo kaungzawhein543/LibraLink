481a2b84f497f4d23e7d19e778ead79049d1ed16
Kaung Zaw Hein <kzheindev789@gmail.com>
2024-11-05 07:44:24 +0630

    Create Custom Exception and Global Exception Handler

---
+++ LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/exceptions/CustomExceptions.java
@@ -0,0 +1,29 @@
+package com.elibrary.LibraLink.exceptions;
+
+public class CustomExceptions {
+
+    public static class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message){
+            super(message);
+        }
+    }
+
+    public static class BadRequestException extends RuntimeException {
+        public BadRequestException(String message){
+            super(message);
+        }
+    }
+
+    public static class UnAuthorizedException extends RuntimeException {
+        public UnAuthorizedException(String message){
+            super(message);
+        }
+    }
+
+    public static class ForbiddenException extends RuntimeException {
+        public ForbiddenException(String message){
+            super(message);
+        }
+    }
+
+}
---
+++ LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.elibrary.LibraLink.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomExceptions.ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(CustomExceptions.ResourceNotFoundException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CustomExceptions.BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(CustomExceptions.BadRequestException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomExceptions.UnAuthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnAuthorized(CustomExceptions.UnAuthorizedException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED.value()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(CustomExceptions.ForbiddenException.class)
+    public ResponseEntity<ErrorResponse> handleForbidden(CustomExceptions.ForbiddenException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now(),
+                HttpStatus.FORBIDDEN.value()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "An Error occurred: " +ex.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value()
+        );
+        return new ResponseEntity<>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
