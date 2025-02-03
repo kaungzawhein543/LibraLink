ae5a0e85fc2846aaa60e5cd196aa40571625d5d6
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-03 23:48:40 +0630

    checkPasswordLength method is for check the password length

--- LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/services/CommonCheck.java
+++ LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/services/CommonCheck.java
@@ -1,4 +1,23 @@
 package com.elibrary.LibraLink.services;
 
 public class CommonCheck {
+
+    // CONSTANT VALUES
+    private final int passwordLength;
+    private static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$";
+
+    // CONSTRUCTOR
+    public CommonCheck(int passwordLength) {
+        this.passwordLength = passwordLength;
+    }
+
+    // CHECK PASSWORD LENGTH
+    private boolean checkPasswordLength(String password) {
+        if (password == null) {
+            return false; // Null passwords are invalid
+        }
+        return password.length() >= passwordLength;
+    }
+
+
 }
