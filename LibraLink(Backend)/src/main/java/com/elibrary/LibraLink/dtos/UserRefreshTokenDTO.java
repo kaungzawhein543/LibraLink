2014333e94b7c93f28718adfd75105dca9214ff5
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-05 23:59:36 +0630

    create DTO Class and implemnt it

--- LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/dtos/UserRefreshTokenDTO.java
+++ LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/dtos/UserRefreshTokenDTO.java
@@ -1,8 +1,20 @@
 package com.elibrary.LibraLink.dtos;
 
+import com.elibrary.LibraLink.entities.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRefreshTokenDTO {
+    private int id;
+    private UUID device_id;
+    private Timestamp expired_at;
+    private Timestamp created_at;
+    private User user_id;
 }
