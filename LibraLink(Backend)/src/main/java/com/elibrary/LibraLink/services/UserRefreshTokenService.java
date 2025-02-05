7b38da144a1b482704f35c9b3ae3b81c45ff3ea3
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-05 23:58:02 +0630

    implement(override) all field from UserRefreshTokenRepo

--- LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/services/UserRefreshTokenService.java
+++ LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/services/UserRefreshTokenService.java
@@ -3,6 +3,8 @@ package com.elibrary.LibraLink.services;
 import com.elibrary.LibraLink.entities.UserRefreshTokens;
 import com.elibrary.LibraLink.repositories.UserRefreshTokenRepository;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.FluentQuery;
 
@@ -41,6 +43,22 @@ public class UserRefreshTokenService implements UserRefreshTokenRepository {
 
     }
 
+    /**
+     * @param integers
+     */
+    @Override
+    public void deleteAllById(Iterable<? extends Integer> integers) {
+
+    }
+
+    /**
+     * @param entities
+     */
+    @Override
+    public void deleteAll(Iterable<? extends UserRefreshTokens> entities) {
+
+    }
+
     @Override
     public void deleteAll() {
 
@@ -56,9 +74,27 @@ public class UserRefreshTokenService implements UserRefreshTokenRepository {
         return List.of();
     }
 
+    /**
+     * @param integers
+     * @return
+     */
+    @Override
+    public List<UserRefreshTokens> findAllById(Iterable<Integer> integers) {
+        return List.of();
+    }
+
     @Override
     public List<UserRefreshTokens> findAll(Sort sort) {
         return List.of();
+    }
+
+    /**
+     * @param pageable
+     * @return
+     */
+    @Override
+    public Page<UserRefreshTokens> findAll(Pageable pageable) {
+        return null;
     }
 
     @Override
@@ -73,6 +109,127 @@ public class UserRefreshTokenService implements UserRefreshTokenRepository {
 
     @Override
     public <S extends UserRefreshTokens, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void flush() {
+
+    }
+
+    /**
+     * @param entity
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends UserRefreshTokens> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    /**
+     * @param entities
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends UserRefreshTokens> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return List.of();
+    }
+
+    /**
+     * @param entities
+     */
+    @Override
+    public void deleteAllInBatch(Iterable<UserRefreshTokens> entities) {
+
+    }
+
+    /**
+     * @param integers
+     */
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Integer> integers) {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    /**
+     * @param integer
+     * @return
+     */
+    @Override
+    public UserRefreshTokens getOne(Integer integer) {
+        return null;
+    }
+
+    /**
+     * @param integer
+     * @return
+     */
+    @Override
+    public UserRefreshTokens getById(Integer integer) {
+        return null;
+    }
+
+    /**
+     * @param integer
+     * @return
+     */
+    @Override
+    public UserRefreshTokens getReferenceById(Integer integer) {
+        return null;
+    }
+
+    /**
+     * @param example
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends UserRefreshTokens> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    /**
+     * @param example
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends UserRefreshTokens> List<S> findAll(Example<S> example) {
+        return List.of();
+    }
+
+    /**
+     * @param example
+     * @param sort
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends UserRefreshTokens> List<S> findAll(Example<S> example, Sort sort) {
+        return List.of();
+    }
+
+    /**
+     * @param example
+     * @param pageable
+     * @param <S>
+     * @return
+     */
+    @Override
+    public <S extends UserRefreshTokens> Page<S> findAll(Example<S> example, Pageable pageable) {
         return null;
     }
 }
