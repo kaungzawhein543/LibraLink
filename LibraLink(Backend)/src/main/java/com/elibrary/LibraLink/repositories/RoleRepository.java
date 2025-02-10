8c30c9fd8d62fe81dee44d3129ec3776e237a733
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-02-10 19:32:42 +0630

    Remove role feature

--- LibraLink(Backend)/src/main/java/com/elibrary/LibraLink/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.elibrary.LibraLink.repositories;
-
-import com.elibrary.LibraLink.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
